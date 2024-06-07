0c87acb34e96f2f81590d8806989d248b63cc410
riguima <richard.alexsander.guima@gmail.com>
2024-06-07 17:38:21 +0000

    fix: arrumado query do admin

--- stories_generator_website/extensions/admin.py
+++ stories_generator_website/extensions/admin.py
@@ -2,6 +2,7 @@ from flask import redirect, url_for
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from flask_login import current_user
+from sqlalchemy import select
 
 from stories_generator_website.database import Session
 from stories_generator_website.models import Product, Configuration
@@ -15,8 +16,18 @@ class AdminModelView(ModelView):
         return redirect(url_for('login'))
 
 
+class ProductModelView(AdminModelView):
+    def get_query(self):
+        return self.session.query(Product).filter(Product.username == current_user.username)
+
+
+class ConfigurationModelView(AdminModelView):
+    def get_query(self):
+        return self.session.query(Configuration).filter(Configuration.username == current_user.username)
+
+
 def init_app(app):
     admin = Admin(app, name='admin')
     session = Session()
-    admin.add_view(AdminModelView(Product, session))
-    admin.add_view(AdminModelView(Configuration, session))
+    admin.add_view(ProductModelView(Product, session))
+    admin.add_view(ConfigurationModelView(Configuration, session))
